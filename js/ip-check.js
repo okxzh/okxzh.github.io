781f7c51e4c0ad9e55c311669e40e09d5aef1280
tggSearch <huangdh4@outlook.com>
2024-11-24 15:31:44 +0800

    u

--- js/ip-check.js
+++ js/ip-check.js
@@ -2,16 +2,17 @@
 fetch('https://api.ip.sb/geoip')
   .then(response => response.json())
   .then(data => {
-    const registerLink = document.getElementById('register-link');
-    const registerButton = document.getElementById('register-button');
-    if (data.country_code === 'CN') {
-      console.log('用户来自中国');
-      registerLink.href = 'https://www.mnftinqq.com/join/CNOFF';
-      registerButton.href = 'https://www.mnftinqq.com/join/CNOFF';
-    } else {
-      console.log('用户来自其他国家');
-      registerLink.href = 'https://www.okx.com/join/CNOFF';
-      registerButton.href = 'https://www.okx.com/join/CNOFF';
+    const registerLinks = document.getElementsByClassName('register-link');
+    const registerButtons = document.getElementsByClassName('register-button');
+    const linkHref = data.country_code === 'CN' 
+      ? 'https://www.mnftinqq.com/join/CNOFF' 
+      : 'https://www.okx.com/join/CNOFF';
+
+    for (let link of registerLinks) {
+      link.href = linkHref;
+    }
+    for (let button of registerButtons) {
+      button.href = linkHref;
     }
   })
   .catch(error => console.error('无法获取用户 IP 信息', error)); 
