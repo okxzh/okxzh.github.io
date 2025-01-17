f393ce4297fca2ffae511ee9a8093674558157e4
tggSearch <huangdh4@outlook.com>
2025-01-17 18:28:23 +0800

    default-okxzh

--- js/ip-check.js
+++ js/ip-check.js
@@ -4,11 +4,11 @@ function setLinks(countryCode) {
   const downloadButtons = document.getElementsByClassName('download-button');
 
   const linkHref = countryCode === 'CN' 
-    ? '/302.html?target=https://www.chouyi.net/join/CNOFF' 
+    ? '/302.html?target=https://www.chouyi.co/join/CNOFF' 
     : '/302.html?target=https://www.okx.com/join/CNOFF';
 
   const downloadLinkHref = countryCode === 'CN' 
-    ? '/302.html?target=https://www.chouyi.net/zh-hans/download' 
+    ? '/302.html?target=https://www.chouyi.co/zh-hans/download' 
     : '/302.html?target=https://www.okx.com/zh-hans/download';
 
   for (let button of registerButtons) {
