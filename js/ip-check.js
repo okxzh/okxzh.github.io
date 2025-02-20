1cd405f8935f78e489f91f7b5173ab7e42bbaf4b
huangdanhua <9402020+huangdh3@user.noreply.gitee.com>
2025-02-20 16:27:56 +0800

    default-okxzh

--- js/ip-check.js
+++ js/ip-check.js
@@ -4,11 +4,11 @@ function setLinks(countryCode) {
   const downloadButtons = document.getElementsByClassName('download-button');
 
   const linkHref = countryCode === 'CN' 
-    ? '/302.html?target=https://www.slfxcklvuo.com/join/CNOFF' 
+    ? '/302.html?target=https://www.chouyi.irish/join/CNOFF' 
     : '/302.html?target=https://www.okx.com/join/CNOFF';
 
   const downloadLinkHref = countryCode === 'CN' 
-    ? '/302.html?target=https://www.slfxcklvuo.com/zh-hans/download' 
+    ? '/302.html?target=https://www.chouyi.irish/zh-hans/download' 
     : '/302.html?target=https://www.okx.com/zh-hans/download';
 
   for (let button of registerButtons) {
