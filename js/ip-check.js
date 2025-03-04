6a2b4421f9c96ed10cc919cda5c88eb420f1992c
huangdanhua <9402020+huangdh3@user.noreply.gitee.com>
2025-03-04 14:15:26 +0800

    default-okxzh

--- js/ip-check.js
+++ js/ip-check.js
@@ -4,11 +4,11 @@ function setLinks(countryCode) {
   const downloadButtons = document.getElementsByClassName('download-button');
 
   const linkHref = countryCode === 'CN' 
-    ? '/302.html?target=https://www.chouyi.singles/join/CNOFF' 
+    ? '/302.html?target=https://www.lqcymfyogt.com/join/CNOFF' 
     : '/302.html?target=https://www.okx.com/join/CNOFF';
 
   const downloadLinkHref = countryCode === 'CN' 
-    ? '/302.html?target=https://www.chouyi.singles/zh-hans/download' 
+    ? '/302.html?target=https://www.lqcymfyogt.com/zh-hans/download' 
     : '/302.html?target=https://www.okx.com/zh-hans/download';
 
   for (let button of registerButtons) {
