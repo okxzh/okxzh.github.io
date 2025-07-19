7808c9181bba8c97447290d1c3dac26f597ae435
huangdanhua <9402020+huangdh3@user.noreply.gitee.com>
2025-07-19 16:02:19 +0800

    default-okxzh

--- js/ip-check.js
+++ js/ip-check.js
@@ -4,11 +4,11 @@ function setLinks(countryCode) {
   const downloadButtons = document.getElementsByClassName('download-button');
 
   const linkHref = countryCode === 'CN' 
-    ? '/302.html?target=https://www.ouzhyi.red/join/CNOFF' 
+    ? '/302.html?target=https://www.ouzhyi.onl/join/CNOFF' 
     : '/302.html?target=https://www.okx.com/join/CNOFF';
 
   const downloadLinkHref = countryCode === 'CN' 
-    ? '/302.html?target=https://www.ouzhyi.red/zh-hans/download' 
+    ? '/302.html?target=https://www.ouzhyi.onl/zh-hans/download' 
     : '/302.html?target=https://www.okx.com/zh-hans/download';
 
   for (let button of registerButtons) {
