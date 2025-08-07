e18081b46b0cb25151c83703334335ec7c924bab
huangdanhua <9402020+huangdh3@user.noreply.gitee.com>
2025-08-07 15:18:21 +0800

    default-okxzh

--- js/ip-check.js
+++ js/ip-check.js
@@ -4,11 +4,11 @@ function setLinks(countryCode) {
   const downloadButtons = document.getElementsByClassName('download-button');
 
   const linkHref = countryCode === 'CN' 
-    ? '/302.html?target=https://www.ouchyi.co/join/CNOFF' 
+    ? '/302.html?target=https://www.oucnyi.cash/join/CNOFF' 
     : '/302.html?target=https://www.okx.com/join/CNOFF';
 
   const downloadLinkHref = countryCode === 'CN' 
-    ? '/302.html?target=https://www.ouchyi.co/zh-hans/download' 
+    ? '/302.html?target=https://www.oucnyi.cash/zh-hans/download' 
     : '/302.html?target=https://www.okx.com/zh-hans/download';
 
   for (let button of registerButtons) {
