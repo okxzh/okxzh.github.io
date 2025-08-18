b2c21d0cca981182dec8b31cc383a1e5d926b74e
huangdanhua <9402020+huangdh3@user.noreply.gitee.com>
2025-08-18 09:20:06 +0800

    default-okxzh

--- js/ip-check.js
+++ js/ip-check.js
@@ -4,7 +4,7 @@ function setLinks(countryCode) {
   const downloadButtons = document.getElementsByClassName('download-button');
 
   const linkHref = countryCode === 'CN' 
-    ? '/302.html?target=https://www.oucnyi.blue/join/CNOFF' 
+    ? '/302.html?target=https://www.oyicn.link/ul/6CngT5?channelId=CNOFF' 
     : '/302.html?target=https://www.okx.com/join/CNOFF';
 
   const downloadLinkHref = countryCode === 'CN' 
